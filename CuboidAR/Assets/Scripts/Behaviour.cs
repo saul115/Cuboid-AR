665e1ac5cec0faf75857b3f551bbd631bb8a9068
Saul Augusto Gasca Farrera <sgfarrera@gmail.com>
2020-03-19 18:57:26 -0600

    Blender Meshes Added

---
+++ CuboidAR/Assets/Scripts/Behaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Behaviour : MonoBehaviour
+{
+
+    Transform rotation;
+
+    private float velocity = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotation = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation.Rotate(Vector3.forward * velocity);
+    }
+}
